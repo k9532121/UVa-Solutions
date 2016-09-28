c98fae16249b56d90a1c9445ad7bc5127120cfc4
k9532121 <k9532121@gmail.com>
2016-09-28 16:17:52 +0800

    add 488 - triangle wave

--- 004 (400-499)/488 - Triangle Wave/main.cpp
+++ 004 (400-499)/488 - Triangle Wave/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
 int main(){
-
+    int n, a, b, i, j, k;
+    cin >> n;
+    while(n-- > 0){
+        cin >> a >> b;
+        for(i=0;i<b;i++){
+            for(j=0;j<a;j++){
+                for(k=0;k<j+1;k++)
+                    cout << j+1;
+                cout << endl;
+            }
+            for(j=a-2;j>=0;j--){
+                for(k=0;k<j+1;k++)
+                    cout << j+1;
+                cout << endl;
+            }
+            if(!(i==b-1 && n==0))cout << endl;
+        }
+    }
     return 0;
 }
